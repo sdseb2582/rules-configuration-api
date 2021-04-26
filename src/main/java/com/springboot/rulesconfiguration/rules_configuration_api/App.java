2217fb6f64bc4d469a42d6a99d27833f961bc60f
Monish <moreddy@galaxe.com>
2021-04-26 11:11:18 -0400

    Initial commit

---
+++ src/main/java/com/springboot/rulesconfiguration/rules_configuration_api/App.java
@@ -0,0 +1,11 @@
+package com.springboot.rulesconfiguration.rules_configuration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
