da32bb0824e2f2d54e402903fbf05ac78d18810d
Lionel Beato <lionel.beato@gmail.com>
2021-05-19 08:48:31 -0400

    comment out lombok, add xml EE dependency, app launches

--- src/main/java/com/springboot/rulesconfiguration/rules_configuration_api/AppConfig.java
+++ src/main/java/com/springboot/rulesconfiguration/rules_configuration_api/AppConfig.java
@@ -11,26 +11,26 @@ import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class AppConfig {
-    @Autowired
-    DataSourceProperties dataSourceProperties;
-
-    @Bean
-    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
-    DataSource realDataSource() {
-        DataSource dataSource = DataSourceBuilder
-                .create(this.dataSourceProperties.getClassLoader())
-                .url(this.dataSourceProperties.getUrl())
-                .username(this.dataSourceProperties.getUsername())
-                .password(this.dataSourceProperties.getPassword())
-                .build();
-        return dataSource;
-    }
-
-    @Bean
-    @Primary
-    DataSource dataSource() {
-        return new DataSourceSpy(realDataSource());
-    }
-}
+//@Configuration
+//public class AppConfig {
+//    @Autowired
+//    DataSourceProperties dataSourceProperties;
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
+//    DataSource realDataSource() {
+//        DataSource dataSource = DataSourceBuilder
+//                .create(this.dataSourceProperties.getClassLoader())
+//                .url(this.dataSourceProperties.getUrl())
+//                .username(this.dataSourceProperties.getUsername())
+//                .password(this.dataSourceProperties.getPassword())
+//                .build();
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    @Primary
+//    DataSource dataSource() {
+//        return new DataSourceSpy(realDataSource());
+//    }
+//}
--- src/main/java/com/springboot/rulesconfiguration/rules_configuration_api/HelloController.java
+++ src/main/java/com/springboot/rulesconfiguration/rules_configuration_api/HelloController.java
@@ -1,6 +1,6 @@
 package com.springboot.rulesconfiguration.rules_configuration_api;
 
-import lombok.Data;
+//import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -18,20 +19,20 @@ public class HelloController {
         return "Hello World!";
     }
 
-    @Data
-    static class Result {
-        private final int left;
-        private final int right;
-        private final long answer;
-    }
+//    @Data
+//    static class Result {
+//        private final int left;
+//        private final int right;
+//        private final long answer;
+//    }
 
     // SQL sample
-    @RequestMapping("calc")
-    Result calc(@RequestParam int left, @RequestParam int right) {
-        MapSqlParameterSource source = new MapSqlParameterSource()
-                .addValue("left", left)
-                .addValue("right", right);
-        return jdbcTemplate.queryForObject("SELECT :left + :right AS answer", source,
-                (rs, rowNum) -> new Result(left, right, rs.getLong("answer")));
-    }
+//    @RequestMapping("calc")
+//    Result calc(@RequestParam int left, @RequestParam int right) {
+//        MapSqlParameterSource source = new MapSqlParameterSource()
+//                .addValue("left", left)
+//                .addValue("right", right);
+//        return jdbcTemplate.queryForObject("SELECT :left + :right AS answer", source,
+//                (rs, rowNum) -> new Result(left, right, rs.getLong("answer")));
+//    }
 }
